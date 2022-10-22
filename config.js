ca6017d68272db5fb5c065d9e5539da9e3f7fc12
Lucas Nascimento <lucasjoao85@gmail.com>
2022-10-21 21:39:22 -0300

    Adicionando channel log

--- config.js
+++ config.js
@@ -4,5 +4,6 @@ module.exports = {
     "clientId": process.env.CLIENDID,
     "guildId": process.env.GUILDID,
     "token": process.env.TOKEN,
-    "corleoneApi":process.env.CORLEONE_API
+    "corleoneApi":process.env.CORLEONE_API,
+    "channel_log":process.env.CHANNEL_LOGS
 }
--- index.js
+++ index.js
@@ -1,12 +1,12 @@
 const { Client, GatewayIntentBits } = require('discord.js');
-const { token } = require('./config.js');
+const { channel_log } = require('./config.js');
 
 const messageCreate = require('./src/controller/messageCreate');
 const registerMember = require('./src/controller/registerMember');
 
 const client = new Client({ intents: [GatewayIntentBits.Guilds, GatewayIntentBits.GuildMessages, GatewayIntentBits.MessageContent] });
 
-const CHANNEL_CHEST = "1031702372516372552";
+const CHANNEL_LOGS = channel_log;
 
 client.once('ready', () => {
     console.log('============ Bot Iniciado ===============');
@@ -14,9 +14,9 @@ client.once('ready', () => {
 });
 
 client.on("messageCreate", (message) => {
-    if (message.channelId === CHANNEL_CHEST) {
+    if (message.channelId === CHANNEL_LOGS) {
         messageCreate.handleMessage(message, (apiMsg) => {
-            client.channels.cache.get(CHANNEL_CHEST).send(apiMsg)
+            client.channels.cache.get(CHANNEL_LOGS).send(apiMsg)
         })
     }
 
